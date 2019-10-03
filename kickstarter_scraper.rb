b4007adecf11535236c5deea43703d00cb3070e1
Jesse Byers <jesse.s.byers@gmail.com>
2019-10-03 14:58:17 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,9 +4,12 @@ require 'pry'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+  projects = {}
 
-
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
+  projects
 end
 
 create_project_hash
@@ -15,3 +18,4 @@ create_project_hash
 #  image link:  project.css("div.project-thumbnail a img").attribute("src").value
 # description:  project.css("p.bbcard_blurb").text
 # location:   project.css("span.location-name").text
+#  percent funded  project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
